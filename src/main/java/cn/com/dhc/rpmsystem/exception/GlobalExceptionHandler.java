e042579c03da32f221578df2c49df9f5e25ba2e9
zss <453615738@qq.com>
2019-08-06 13:15:55 +0800

    异常全局处理

--- src/main/java/cn/com/dhc/rpmsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/cn/com/dhc/rpmsystem/exception/GlobalExceptionHandler.java
@@ -3,15 +3,12 @@ package cn.com.dhc.rpmsystem.exception;
 import cn.com.dhc.rpmsystem.common.ErrorCode;
 import cn.com.dhc.rpmsystem.entity.ResultEntity;
 import cn.com.dhc.rpmsystem.utils.ResultUtils;
-import io.swagger.models.auth.In;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /**
  * 实现全局的错误处理
@@ -26,17 +23,18 @@ public class GlobalExceptionHandler {
 
     //自定义异常
     @ExceptionHandler(Exception.class)
-    public ResultEntity handleException(Exception e, HttpServletRequest request, HttpServletResponse response){
+    public ResultEntity handleException(Exception e, HttpServletRequest request){
 
         logger.error("请求：{}发生异常：{}", request.getRequestURI(), e);
 
-        ResultEntity result = null;
+        ResultEntity result;
         if (e instanceof BusinessException) {
             result = ResultUtils.failure( ((BusinessException) e).getBusinessMsg(), Integer.valueOf(((BusinessException) e).getErrorCode()));
         } else {
             result = ResultUtils.failure(ErrorCode.ERROR.getMsg(), Integer.valueOf(ErrorCode.ERROR.getCode()));
         }
 
+        result.setData(new ArrayList<>());
         return result;
     }
 
