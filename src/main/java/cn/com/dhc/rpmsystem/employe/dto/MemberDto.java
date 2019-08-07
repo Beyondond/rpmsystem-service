b156303a53b8b68ad3d44720221bf814ea3793cf
zss <453615738@qq.com>
2019-08-07 15:15:17 +0800

    去除多余代码

--- src/main/java/cn/com/dhc/rpmsystem/employe/dto/MemberDto.java
+++ src/main/java/cn/com/dhc/rpmsystem/employe/dto/MemberDto.java
@@ -1,11 +1,9 @@
 package cn.com.dhc.rpmsystem.employe.dto;
 
-import cn.com.dhc.rpmsystem.entity.Order;
 import cn.com.dhc.rpmsystem.entity.ReqPage;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.Serializable;
 
 /**
  * @author zss
@@ -15,7 +13,6 @@ import java.io.Serializable;
 @Setter
 public class MemberDto extends ReqPage {
 
-    private static final long serialVersionUID = -5392034203655686144L;
 
     /**
      * 检索姓名
