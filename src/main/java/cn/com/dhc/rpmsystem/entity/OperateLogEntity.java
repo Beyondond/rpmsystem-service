0fbea27e8095b7fc7b01bf5f61ca92a6b2870c96
yongzhouzhou <18292098690@163.com>
2019-08-08 14:40:10 +0800

    员工信息实体类

--- src/main/java/cn/com/dhc/rpmsystem/entity/OperateLogEntity.java
+++ src/main/java/cn/com/dhc/rpmsystem/entity/OperateLogEntity.java
@@ -6,27 +6,31 @@ import org.apache.commons.lang3.StringUtils;
 
 /**
  * 操作日志实体对象
+ *
  * @author Sunli
  */
 @Setter
 @Getter
-public class OperateLogEntity
-{
-	// 日志Id
-	private Integer id;
-	
-	// 操作对象技能
-	private Integer skill;
-	
-	// 操作内容
-	private String operate_content = StringUtils.EMPTY;
-	
-	// 操作结果 默认成功
-	private Integer operate_result = 1;
-	
-	// 操作人
-	private Integer operate_num_uid;
-	
-	// 操作时间
-	private String operate_time = StringUtils.EMPTY;
+public class OperateLogEntity {
+    // 日志Id
+    private Integer id;
+
+    // 操作对象技能
+    private String skill;
+
+    // 操作内容
+    private String operate_content = StringUtils.EMPTY;
+
+    // 操作结果 默认成功
+    private Integer operate_result = 1;
+
+    // 操作人
+    private Integer operate_num_uid;
+
+    // 操作时间
+    private String operate_time = StringUtils.EMPTY;
+    /**
+     * 操作人名字
+     */
+    private String mem_name;
 }
