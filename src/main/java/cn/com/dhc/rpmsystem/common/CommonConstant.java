289a77353f05309e9557c7e889a4f36c3308f14a
zss <453615738@qq.com>
2019-08-08 08:58:52 +0800

    员工首页展示列

--- src/main/java/cn/com/dhc/rpmsystem/common/CommonConstant.java
+++ src/main/java/cn/com/dhc/rpmsystem/common/CommonConstant.java
@@ -60,4 +60,54 @@ public class CommonConstant  {
         }
     }
 
+
+    /**
+     * 员工首页展示列
+     */
+    public enum ColumnShow implements EnumInterface {
+        numUid(1, "工号"),
+        memName(2, "姓名"),
+        memGender(3, "性别"),
+        memLevel(4, "级别"),
+        positon(5, "职位"),
+        education(6, "学历"),
+        major(7, "专业"),
+        engLevel(8, "英语等级"),
+        graSchool(9, "毕业学校"),
+        graTime(10, "毕业时间"),
+        initPositionTime(11, "入职时间"),
+        initProjectTime(12, "入项时间"),
+        technologyType(13, "技术类型"),
+        platformSystem(14, "平台系统"),
+        phoneNum(15, "电话号码"),
+        identityId(16, "身份证号"),
+        mailbox(17, "个人邮箱"),
+        technologyYears(18, "技术年限"),
+        hwExp(19, "是否有华为经验"),
+        pastCorp(20, "过往公司"),
+        projectExp(21, "项目经验"),
+        inputNumUid(22, "录入人员工号"),
+        inputTime(23, "录入时间"),
+        positionStatus(24, "是否在职");
+
+
+        Integer value;
+        String desc;
+
+        ColumnShow(Integer value, String desc) {
+            this.value = value;
+            this.desc = desc;
+        }
+
+        @Override
+        public Integer getValue() {
+            return value;
+        }
+
+        @Override
+        public String getDesc() {
+            return desc;
+        }
+    }
+
 }
