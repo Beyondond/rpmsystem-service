21ce819df3d02dad2c0d706635554b3cd8494024
zss <453615738@qq.com>
2019-08-08 09:16:25 +0800

    修改文件名称

--- src/main/java/cn/com/dhc/rpmsystem/system/dao/RpmMemberRoleDao.java
+++ src/main/java/cn/com/dhc/rpmsystem/system/dao/RpmMemberRoleDao.java
@@ -3,5 +3,5 @@ package cn.com.dhc.rpmsystem.system.dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RpmMemberRoleMapper {
+public interface RpmMemberRoleDao {
 }
